f03fc2ffa769e696289c7535155f285980eab5dd
IlanRubio <ilanrubio4@gmail.com>
2023-06-02 18:16:52 +0200

    Creation de la classe test Scenario

---
+++ SAE_ALGO_2_02/src/main/java/modele/ScenarioTest.java
@@ -0,0 +1,23 @@
+package modele;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * La classe ScenarioTest permet de tester la classe Scenario
+ */
+class ScenarioTest {
+
+    @org.junit.jupiter.api.Test
+    /**
+     Méthode de test de la méthode ajoutQuete de la classe Scenario.
+     */
+    void ajoutQuete() {
+    }
+
+    @org.junit.jupiter.api.Test
+    /**
+     Méthode de test de la méthode getQuete de la classe Scenario.
+     */
+    void getQuete() {
+    }
+}
