6e2efecc8c64133473f144f24ffc804d624af562
IlanRubio <ilanrubio4@gmail.com>
2023-06-06 09:46:10 +0200

    Ajout de la méthode calculerDistance

--- SAE_ALGO_2_02/src/main/java/modele/Solution.java
+++ SAE_ALGO_2_02/src/main/java/modele/Solution.java
@@ -1,6 +1,7 @@
 package modele;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * La classe Solution représente une solution avec son scénario et son joueur associés.
@@ -8,6 +9,7 @@ import java.util.ArrayList;
 public class Solution {
     private Scenario chScenario;
     private Joueur chJoueur;
+    private boolean chQueteFinal = false;
 
     /**
      * Constructeur de la classe Solution.
@@ -22,11 +24,25 @@ public class Solution {
 
     /**
      * Effectue une recherche efficace pour trouver une solution optimale en terme de déplacement.
+     * Met à jour les valeurs de l'objet Joueur
      *
-     * @return Une liste d'entiers représentant toutes les quetes à réaliser et dans quel ordre.
      */
-    public ArrayList<Integer> efficace() {
-        return null;
+    public void efficace() {
+
+
+    }
+
+    /**
+     * Calcule la distance entre la quete et la position du joueur.
+     *
+     * @param pos1 Les coordonnées de la première position [x, y].
+     * @param pos2 Les coordonnées de la deuxième position [x, y].
+     * @return La distance entre les deux positions.
+     */
+    private int calculerDistance(int[] pos1, int[] pos2) {
+        int dx = pos2[0] - pos1[0];
+        int dy = pos2[1] - pos1[1];
+        return (int) Math.sqrt(dx * dx + dy * dy);
     }
 
     /**
