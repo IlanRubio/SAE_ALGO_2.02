accb8d13707e8ed39385876bd29e8aebce73e6d5
IlanRubio <ilanrubio4@gmail.com>
2023-06-06 14:14:35 +0200

    Ajout des constances pour les différents types de solutions

--- SAE_ALGO_2_02/src/main/java/modele/ConstanteScenario.java
+++ SAE_ALGO_2_02/src/main/java/modele/ConstanteScenario.java
@@ -7,4 +7,9 @@ public interface ConstanteScenario {
      * Les scenario disponibles.
      */
     final String[] SCENARIO = {"Scenario 0","Scenario 1","Scenario 2","Scenario 3","Scenario 4","Scenario 5","Scenario 6","Scenario 7","Scenario 8","Scenario 9","Scenario 10"};
+
+    /**
+     * Les solutions disponibles.
+     */
+    final String[] SOLUTION = {"Efficace", "Exhaustive"};
 }
