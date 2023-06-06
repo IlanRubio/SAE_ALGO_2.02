a4b55b3e16a55bc16580b4a01a85113e2ec4ecb6
IlanRubio <ilanrubio4@gmail.com>
2023-06-06 10:50:45 +0200

    Maj de Client afin de voir des données dans le terminal

--- SAE_ALGO_2_02/src/main/java/modele/Client.java
+++ SAE_ALGO_2_02/src/main/java/modele/Client.java
@@ -8,12 +8,28 @@ import java.io.File;
 public class Client {
     static Scenario sc;
     static Joueur j1 = new Joueur();
+    static Joueur j2 = new Joueur();
     public static void main(String[] args) {
-        File file = new File("SAE_ALGO_2_02"+File.separator+"Scenarios"+File.separator+"scenario_0.txt");
+        File file = new File("SAE_ALGO_2_02"+File.separator+"Scenarios"+File.separator+"scenario_4.txt");
         sc = LectureFichierQuete.lecture(file);
         Solution solutionEfficace = new Solution(sc, j1);
         solutionEfficace.efficace();
-        System.out.println(j1.getQueteAcc());
+        System.out.println("Solution efficace : ");
+        System.out.println("Total xp : " + j1.getExpJ());
+        System.out.println("Total durée : " + j1.getDureeJ());
+        System.out.println("Nombre de quetes realisees : " + j1.getQueteAcc().size());
+        for (Quete quete : j1.getQueteAcc()){
+            System.out.println(quete);
+        }
+        Solution solutionExhaustive = new Solution(sc, j2);
+        solutionExhaustive.exhaustive();
+        System.out.println("Solution exhaustive : ");
+        System.out.println("Total xp : " + j2.getExpJ());
+        System.out.println("Total durée : " + j2.getDureeJ());
+        System.out.println("Nombre de quetes realisees : " + j2.getQueteAcc().size());
+        for (Quete quete : j2.getQueteAcc()){
+            System.out.println(quete);
+        }
     }
 }
 
