1d0abc60dd4652eb0a10259a740965f08145961d
IlanRubio <ilanrubio4@gmail.com>
2023-06-06 19:33:16 +0200

    Modification de la classe lecture de la classe LectureFichierQuete

--- SAE_ALGO_2_02/src/main/java/modele/LectureFichierQuete.java
+++ SAE_ALGO_2_02/src/main/java/modele/LectureFichierQuete.java
@@ -2,12 +2,14 @@ package modele;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
  * La classe LectureFichierQuete permet de lire un fichier de quêtes et de retourner un objet Scenario.
  */
 public class LectureFichierQuete {
+    private static Scenario chScenario = new Scenario();
     /**
      * Lit un fichier de quêtes et retourne un objet Scenario correspondant.
      *
@@ -15,18 +17,17 @@ public class LectureFichierQuete {
      * @return Un objet Scenario contenant les quêtes lues.
      */
     public static Scenario lecture(File parFile) {
-        Scenario scenario = new Scenario();
         try{
             Scanner sc = new Scanner(parFile);
             while (sc.hasNext()){
                 Quete q = new Quete(sc.nextLine());
-                scenario.ajoutQuete(q);
+                chScenario.ajoutQuete(q);
             }
         }
         catch (FileNotFoundException e ){
             System.err.println(e.getMessage());
         }
-        return scenario;
+        return chScenario;
     }
 }
 
