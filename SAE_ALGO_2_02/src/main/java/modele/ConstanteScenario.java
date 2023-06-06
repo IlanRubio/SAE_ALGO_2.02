5b5105ebe362cede6ee784d1861b2c40d68e1de0
IlanRubio <ilanrubio4@gmail.com>
2023-06-06 13:46:51 +0200

    Creation de l'interface de constante necessaire pour l'application

---
+++ SAE_ALGO_2_02/src/main/java/modele/ConstanteScenario.java
@@ -0,0 +1,10 @@
+package modele;
+/**
+ * L'interface ConstantesPlanning contient les constantes utilisées dans l'application'.
+ */
+public interface ConstanteScenario {
+    /**
+     * Les scenario disponibles.
+     */
+    final String[] SCENARIO = {"Scenario 0","Scenario 1","Scenario 2","Scenario 3","Scenario 4","Scenario 5","Scenario 6","Scenario 7","Scenario 8","Scenario 9","Scenario 10"};
+}
