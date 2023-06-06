8afc66bdea35e967008604338a5fddb4b1ec8604
IlanRubio <ilanrubio4@gmail.com>
2023-06-06 11:37:03 +0200

    Création de la classe test de Solution

---
+++ SAE_ALGO_2_02/src/main/java/modele/SolutionTest.java
@@ -0,0 +1,22 @@
+package modele;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+    @org.junit.jupiter.api.Test
+    /**
+     Méthode de test de la méthode efficace de la classe Solution.
+     */
+    void efficace() {
+    }
+
+    @org.junit.jupiter.api.Test
+    /**
+     Méthode de test de la méthode exhaustive de la classe Solution.
+     */
+    void exhaustive() {
+    }
+}
