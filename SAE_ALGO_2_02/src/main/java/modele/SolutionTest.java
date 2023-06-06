5d155c47459c6c6d375ee3c287c09d4d7f826f51
IlanRubio <ilanrubio4@gmail.com>
2023-06-06 16:23:59 +0200

    Ajout de la methode test de la methode efficace de la classe Solution

--- SAE_ALGO_2_02/src/main/java/modele/SolutionTest.java
+++ SAE_ALGO_2_02/src/main/java/modele/SolutionTest.java
@@ -2,6 +2,10 @@ package modele;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SolutionTest {
@@ -11,6 +15,22 @@ class SolutionTest {
      Méthode de test de la méthode efficace de la classe Solution.
      */
     void efficace() {
+        Scenario scenar ;
+        Joueur j1 = new Joueur();
+        File file = new File("SAE_ALGO_2_02"+File.separator+"Scenarios"+File.separator+"scenario_0.txt");
+        scenar = LectureFichierQuete.lecture(file);
+        Solution solutionEfficace = new Solution(scenar, j1);
+        solutionEfficace.efficace();
+        Quete quete1 = new Quete("1 - [4, 3] - [0, 0, 0, 0] - 2 - 100 - explorer pic de Bhanborim");
+        Quete quete2 = new Quete("2 - [3, 1] - [1, 0, 0, 0] - 1 - 150 - dialoguer avec Kaela la chaman des esprits");
+        Quete quete3 = new Quete("4 - [3, 2] - [2, 0, 0, 0] - 6 - 100 - vaincre Loup Géant");
+        Quete quete4 = new Quete("0 - [1, 1] - [3, 4, 0, 0] - 4 - 350 - vaincre Araignée lunaire");
+        Quete[] elements = {quete1,quete2,quete3,quete4};
+        ArrayList <Quete> listattendu = new ArrayList<>(Arrays.asList(elements));
+
+        assertEquals(listattendu,j1.getQueteAcc());
+
+
     }
 
     @org.junit.jupiter.api.Test
