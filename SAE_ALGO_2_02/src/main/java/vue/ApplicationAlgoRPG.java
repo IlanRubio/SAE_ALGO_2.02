050062c6c0255d8dce81d284d943e3ac58340395
IlanRubio <ilanrubio4@gmail.com>
2023-06-06 11:25:34 +0200

    Création de la classe permettant de lancer l'application

---
+++ SAE_ALGO_2_02/src/main/java/vue/ApplicationAlgoRPG.java
@@ -0,0 +1,34 @@
+package vue;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * Cette classe représente la deuxième application JavaFX.
+ * Elle hérite de la classe Application et implémente la méthode start.
+ */
+public class ApplicationAlgoRPG extends Application {
+    /**
+     * La méthode start est appelée lors du lancement de l'application.
+     * Elle crée une scène avec un conteneur HBox et l'affiche dans une fenêtre.
+     *
+     * @param stage la fenêtre principale de l'application
+     */
+    public void start(Stage stage) {
+        HBoxRoot root = new HBoxRoot();
+        Scene scene = new Scene(root, 1000, 380);
+        stage.setScene(scene);
+        stage.setTitle("Solution RPG");
+        stage.show();
+    }
+
+    /**
+     * Le point d'entrée de l'application.
+     *
+     * @param args les arguments de la ligne de commande
+     */
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+}
