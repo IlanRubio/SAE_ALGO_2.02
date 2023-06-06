1a5fa8592c236299c0778b9967e57e6065fc8840
IlanRubio <ilanrubio4@gmail.com>
2023-06-06 16:25:08 +0200

    Ajout de la méthode test de la methode exhaustive de la classe Solution

--- SAE_ALGO_2_02/src/main/java/modele/SolutionTest.java
+++ SAE_ALGO_2_02/src/main/java/modele/SolutionTest.java
@@ -38,5 +38,20 @@ class SolutionTest {
      Méthode de test de la méthode exhaustive de la classe Solution.
      */
     void exhaustive() {
+        Scenario scenar ;
+        Joueur j1 = new Joueur();
+        File file = new File("SAE_ALGO_2_02"+File.separator+"Scenarios"+File.separator+"scenario_0.txt");
+        scenar = LectureFichierQuete.lecture(file);
+        Solution solutionExhaustive = new Solution(scenar, j1);
+        solutionExhaustive.exhaustive();
+        Quete quete1 = new Quete("1 - [4, 3] - [0, 0, 0, 0] - 2 - 100 - explorer pic de Bhanborim");
+        Quete quete2 = new Quete("2 - [3, 1] - [1, 0, 0, 0] - 1 - 150 - dialoguer avec Kaela la chaman des esprits");
+        Quete quete3 = new Quete("4 - [3, 2] - [2, 0, 0, 0] - 6 - 100 - vaincre Loup Géant");
+        Quete quete4 = new Quete("3 - [0, 4] - [2, 0, 0, 0] - 3 - 200 - explorer palais de Ahehona");
+        Quete quete5 = new Quete("0 - [1, 1] - [3, 4, 0, 0] - 4 - 350 - vaincre Araignée lunaire");
+        Quete[] elements = {quete1,quete2,quete3,quete4,quete5};
+        ArrayList <Quete> listattendu = new ArrayList<>(Arrays.asList(elements));
+
+        assertEquals(listattendu,j1.getQueteAcc());
     }
 }
