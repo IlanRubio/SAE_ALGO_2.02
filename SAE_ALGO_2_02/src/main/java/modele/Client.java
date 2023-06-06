10b07449ce972e90d9eba66fc649ff77b9f9cdbe
IlanRubio <ilanrubio4@gmail.com>
2023-06-06 09:52:57 +0200

    Realisation de la méthode main de la classe Client

--- SAE_ALGO_2_02/src/main/java/modele/Client.java
+++ SAE_ALGO_2_02/src/main/java/modele/Client.java
@@ -1,11 +1,19 @@
 package modele;
 
+import java.io.File;
+
 /**
  * La classe Client est la classe principale du programme.
  */
 public class Client {
+    static Scenario sc;
+    static Joueur j1 = new Joueur();
     public static void main(String[] args) {
-
+        File file = new File("SAE_ALGO_2_02"+File.separator+"Scenarios"+File.separator+"scenario_0.txt");
+        sc = LectureFichierQuete.lecture(file);
+        Solution solutionEfficace = new Solution(sc, j1);
+        solutionEfficace.efficace();
+        System.out.println(j1.getQueteAcc());
     }
 }
 
