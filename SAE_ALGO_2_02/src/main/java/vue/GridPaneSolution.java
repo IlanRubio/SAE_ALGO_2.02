697c2b4c37cab1c2a9762c023e5014d5db84cbb3
IlanRubio <ilanrubio4@gmail.com>
2023-06-06 11:31:18 +0200

    Création de la classe GridPaneSolution contenant les boutons pour faire choix

---
+++ SAE_ALGO_2_02/src/main/java/vue/GridPaneSolution.java
@@ -0,0 +1,12 @@
+package vue;
+
+import javafx.scene.control.ComboBox;
+import javafx.scene.layout.GridPane;
+/**
+ * Cette classe représente un formulaire de réservation sous forme de grille.
+ * Elle hérite de la classe GridPane.
+ */
+public class GridPaneSolution extends GridPane {
+    private ComboBox chChoixScenario;
+
+}
