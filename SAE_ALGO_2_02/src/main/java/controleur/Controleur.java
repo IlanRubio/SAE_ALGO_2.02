802a060ddc8eee12a25849636355cea4eae7a15c
IlanRubio <ilanrubio4@gmail.com>
2023-06-06 21:49:37 +0200

    Creation du Controleur

---
+++ SAE_ALGO_2_02/src/main/java/controleur/Controleur.java
@@ -0,0 +1,12 @@
+package controleur;
+
+import javafx.event.Event;
+import javafx.event.EventHandler;
+
+public class Controleur implements EventHandler {
+
+    @Override
+    public void handle(Event event) {
+
+    }
+}
