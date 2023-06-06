950f8e4e2b1b5d7aec478bbb7452d763f5f0b967
IlanRubio <ilanrubio4@gmail.com>
2023-06-06 14:30:42 +0200

    Ajout de la methode test de la methode getNombreQuetes de la classe Scenario

--- SAE_ALGO_2_02/src/main/java/modele/ScenarioTest.java
+++ SAE_ALGO_2_02/src/main/java/modele/ScenarioTest.java
@@ -3,6 +3,7 @@ package modele;
 import java.util.TreeMap;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * La classe ScenarioTest permet de tester la classe Scenario
@@ -31,5 +32,22 @@ class ScenarioTest {
         Quete objet = new Quete("1|(3, 1)|()|2|50|dialoguer avec Alaric le mage noir");
         scenarioAttendu.ajoutQuete(objet);
         assertEquals(scenarioAttendu.getQuete(1),objet);
+    }
+
+    @org.junit.jupiter.api.Test
+    /**
+     Méthode de test de la méthode getNombreQuetes de la classe Scenario.
+     */
+    void getNombreQuetes() {
+        Scenario scenarioAttendu = new Scenario();
+        assertEquals(0,scenarioAttendu.getNombreQuetes());
+
+        Quete quete1 = new Quete("1|(3, 1)|()|2|50|dialoguer avec Alaric le mage noir");
+        scenarioAttendu.ajoutQuete(quete1);
+        assertEquals(1,scenarioAttendu.getNombreQuetes());
+
+        Quete quete2 = new Quete("2|(3, 1)|((1,),)|1|150|dialoguer avec Kaela la chaman des esprits");
+        scenarioAttendu.ajoutQuete(quete2);
+        assertEquals(2,scenarioAttendu.getNombreQuetes());
     }
 }
